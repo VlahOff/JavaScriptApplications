91851a4f87394f8ea8e792efee3f6a888f1aac57
Dimitar Vlahov <88899711+VlahOff@users.noreply.github.com>
2022-07-04 14:18:49 +0300

    .

--- AsynchronousProgrammingLab/P03GighubCommits_ASYNC/app.js
+++ AsynchronousProgrammingLab/P03GighubCommits_ASYNC/app.js
@@ -1,8 +1,23 @@
-function loadCommits() {
+async function loadCommits() {
     const username = document.getElementById('username').value;
     const repo = document.getElementById('repo').value;
     const list = document.getElementById('commits');
 
-    fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
-    
+    try {
+        const response = await fetch(`https://api.github.com/repos/${username}/${repo}/commits`);
+        if (!response.ok) {
+            throw new Error(`${response.status}(${response.statusText})`);
+        }
+
+        const data = await response.json()
+        for (const commit of data) {
+            const li = document.createElement('li');
+            const p = document.createElement('p');
+            p.textContent = `${commit.commit.author.name}: ${commit.commit.message}`;
+            li.appendChild(p);
+            list.appendChild(li);
+        }
+    } catch (error) {
+        list.innerHTML = `${error}`
+    }
 }
