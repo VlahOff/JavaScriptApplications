ca9d350552b759a12fc4d58c3cb5add2429d8dfd
VlahOff <dimitarvlahov24@gmail.com>
2022-07-07 17:51:27 +0300

    .

--- AsynchronousProgrammingExercise/P02BusSchedule/app.js
+++ AsynchronousProgrammingExercise/P02BusSchedule/app.js
@@ -1,11 +1,29 @@
 function solve() {
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const infoBox = document.querySelector('span.info');
+
+    let busStop = {
+        next: 'depot',
+    };
 
     function depart() {
-        console.log('Depart TODO...');
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${busStop.next}`)
+            .then(response => response.json())
+            .then((data) => {
+                // we break the reference using this
+                busStop = JSON.parse(JSON.stringify(data));
+                infoBox.textContent = `Next stop ${busStop.name}`;
+            })
+            .catch(error => console.log(error));
+            arriveBtn.disabled = false;
+            departBtn.disabled = true;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoBox.textContent = `Arriving at ${busStop.name}`;
+        arriveBtn.disabled = true;
+        departBtn.disabled = false;
     }
 
     return {
