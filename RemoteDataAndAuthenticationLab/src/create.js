ae5f364236c0c2bc9a5bf55ffd9144cbd899bd67
VlahOff <dimitarvlahov24@gmail.com>
2022-07-15 17:00:42 +0300

    .

--- RemoteDataAndAuthenticationLab/src/create.js
+++ RemoteDataAndAuthenticationLab/src/create.js
@@ -3,10 +3,46 @@ document.querySelector('form').addEventListener('submit', onCreate);
 async function onCreate(event) {
     event.preventDefault();
 
+    const formData = new FormData(event.target);
+
+
+    const name = formData.get('name').trim();
+    const img = formData.get('img').trim();
+    const ingredients = formData.get('ingredients').trim().split('\n');
+    const steps = formData.get('steps').trim().split('\n');
+
     const recipe = {
-        name: document.querySelector('input[name="name"]');
+        name,
+        img,
+        ingredients,
+        steps
+    };
+
+    const token = sessionStorage.getItem('accessToken');
+
+    if (token == null) {
+        alert('Please login');
+        window.localStorage = '/RemoteDataAndAuthenticationLab/login.html';
+        return;
     }
 
-    console.log(recipe)
+    try {
+        const response = await fetch('http://localhost:3030/jsonstore/cookbook/recipes', {
+            method: 'post',
+            headers: {
+                'Content-type': 'application/json',
+                'X-Authorization': token
+            },
+            body: JSON.stringify(recipe)
+        });
 
+        if (!response.ok) {
+            const error = await response.json();
+            throw new Error(error);
+        }
+
+        window.location = '/RemoteDataAndAuthenticationLab/index.html';
+    } catch (error) {
+        alert(error.message);
+    }
 }
