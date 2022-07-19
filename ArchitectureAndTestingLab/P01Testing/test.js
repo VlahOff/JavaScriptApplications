578be6d95a63d66107dfa99a7d1cd42ee7e876aa
VlahOff <dimitarvlahov24@gmail.com>
2022-07-19 21:51:05 +0300

    .

--- ArchitectureAndTestingLab/P01Testing/test.js
+++ ArchitectureAndTestingLab/P01Testing/test.js
@@ -11,8 +11,33 @@ describe('E2E tests', async function () {
     beforeEach(async () => { page = await browser.newPage(); });
     afterEach(async () => { await page.close(); });
 
-    it('works', async () => {
+    it('loads article titles', async () => {
         await page.goto('http://127.0.0.1:5500/P01Accordion');
-        await page.screenshot({path: 'site.png'});
-    })
+        await page.waitForSelector('.accordion');
+        const content = await page.textContent('#main');
+        expect(content).to.contain('Scalable Vector Graphics');
+        expect(content).to.contain('Open standard');
+        expect(content).to.contain('Unix');
+        expect(content).to.contain('ALGOL');
+    });
+
+    it('loads articles', async () => {
+        await page.goto('http://127.0.0.1:5500/P01Accordion');
+        await page.click('text=more');
+        await page.waitForSelector('.accordion p');
+        const visible = await page.isVisible('.accordion p');
+        expect(visible).to.be.true;
+    });
+
+    it('hides articles', async () => {
+        await page.goto('http://127.0.0.1:5500/P01Accordion');
+
+        await page.click('text=more');
+        await page.waitForSelector('.accordion p');
+        let visible = await page.isVisible('.accordion p');
+
+        await page.click('text = less');
+        visible = await page.isVisible('.accordion p')
+        expect(visible).to.be.false;
+    });
 });
